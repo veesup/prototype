0b960343f987f06004042828c581ac35a896debe
vee sup <veenakornofficial@gmail.com>
2022-10-26 15:52:24 +0700

    fixxer

---
+++ Models/IProduct.cs
@@ -0,0 +1,10 @@
+﻿using Models;
+using System.Collections.Generic;
+
+namespace Interfaces
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAllClients(bool trackChanges);
+    }
+}
